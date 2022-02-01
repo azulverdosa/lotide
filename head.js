a04ed530d9561a5f432e8f36f21e61d80db39c3e
AvaCado <eliserichardsson@gmail.com>
2022-02-01 11:37:07 -0500

    missed line 13 - completed

--- head.js
+++ head.js
@@ -10,7 +10,7 @@ const _ = require('lodash');
 // to just import the head method of lodash destructure:
 // const { head } = require('lodash');
 //  const headder = head(arr);
-
+const head =(arr) => {
   const headder = _.head(arr);
   return headder;
 }
