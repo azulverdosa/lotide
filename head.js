ed7779cd38557f741838f8bfcbbb5911cb532663
AvaCado <eliserichardsson@gmail.com>
2022-02-01 11:32:52 -0500

    lodash head method notes

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = (actual, expected) => { 
+  console.log(
+    actual === expected
+    ? "Assertion Passed: " + String.fromCodePoint(0x2705) + ` ${actual} === ${expected}`
+    : "Assertion Failed: " + String.fromCodePoint(0x274C) + ` ${actual} !== ${expected}`
+  );
+}
+
+const _ = require('lodash');
+// to just import the head method of lodash destructure:
+// const { head } = require('lodash');
+//  const headder = head(arr);
+
+  const headder = _.head(arr);
+  return headder;
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
