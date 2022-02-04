cd1aa617a6e0587fe0d0844594a25d932f7add05
AvaCado <eliserichardsson@gmail.com>
2022-02-04 12:52:40 -0500

    complete with help from Andy - almost got this one on my own

--- eqObjects.js
+++ eqObjects.js
@@ -9,31 +9,43 @@ const assertEqual = (actual, expected) => {
   );
 }
 
+const eqArrays = (arr1, arr2) => {
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+
 const eqObjects = function(object1, object2) {
   let obj1Arr = Object.keys(object1);
   let obj2Arr = Object.keys(object2);
 
-  if(obj1Arr.length !== obj2Arr.length) {
-    return false;
-  } else {
-    for (let keys of obj1Arr) {
-      if(object1[keys] === object2[keys]) {
-        return true;
-      } else {
+  if(obj1Arr.length === obj2Arr.length) {
+    for (let key of obj1Arr) {
+      // if (JSON.stringify(object1[key]) !== JSON.stringify(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    } 
-  } 
+    }
+
+    return true;
+  }
+
+  return false;
 }
 
 //test cases:
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
-assertEqual(eqObjects(ab, ba), true);
+// eqObjects(ab, ba); // => true
+assertEqual(eqObjects(ab, ba), true, 'first');
 
 const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
-assertEqual(eqObjects(ab, abc), false);
+// eqObjects(ab, abc); // => false
+assertEqual(eqObjects(ab, abc), false, 'second');
 
-console.log(eqObjects(ba, abc))
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+// eqObjects(cd, dc); // => true
+assertEqual(eqObjects(cd, dc), true, 'third');
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+// eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, cd2), false, 'fourth');
